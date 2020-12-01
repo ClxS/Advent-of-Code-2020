6948f0fedcb99cdc72ed848b7b6e5536e92579f7
Carl <slxxls92@gmail.com>
2020-12-01 21:49:34 +0000

    Create Part2SolverLinq.cs

---
+++ Source/Day-01/Solution/Part2SolverLinq.cs
@@ -0,0 +1,37 @@
+﻿namespace Day1
+{
+    using Common;
+    using Serilog;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Part2SolverLinq : ISolver
+    {
+        private readonly int target;
+        private readonly int[] inputs;
+
+        public Part2SolverLinq(int target, params int[] inputs)
+        {
+            this.target = target;
+            this.inputs = inputs;
+        }
+
+        public string Name => "Day1 Part2 LinqHorror";
+
+        public void Solve()
+        {
+            var store = this.inputs.ToHashSet();
+
+            var match =
+                from idx1 in Enumerable.Range(0, this.inputs.Length)
+                from idx2 in Enumerable.Range(idx1, this.inputs.Length - idx1)
+                where store.Contains(this.target - (this.inputs[idx1] + this.inputs[idx2]))
+                select (this.inputs[idx1] * this.inputs[idx2] * (this.target - (this.inputs[idx1] + this.inputs[idx2])));
+
+            if (match.Any())
+            {
+                Log.Information("{D}", match.First());
+            }
+        }
+    }
+}
