cc54d56bc8a92ebca6eb945c026f8c164b30cbe9
Carl <slxxls92@gmail.com>
2020-12-01 06:14:18 +0000

    Program shell now configures the logger

--- Source/Common/ProgramShell.cs
+++ Source/Common/ProgramShell.cs
@@ -1,11 +1,18 @@
 ﻿namespace Common
 {
+    using Serilog;
     using System.Threading.Tasks;
 
     public static class ProgramShell
     {
-        public static Task RunAsync(params ISolver[] solvers)
+        public static async Task RunAsync(params ISolver[] solvers)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .CreateLogger();
+
             foreach (var solver in solvers)
             {
                 await solver.SolveAsync().ConfigureAwait(false);
