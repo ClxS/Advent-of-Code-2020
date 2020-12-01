a917583413e276d2ceeaa146018f2bdbd978c4e2
Carl <slxxls92@gmail.com>
2020-12-01 06:16:00 +0000

    Added util for turning data into int arrays

---
+++ Source/Common/FileUtil.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    using System.IO;
+    using System.Linq;
+
+    public static class FileUtil
+    {
+        public static int[] GetIntArray(string filePath)
+        {
+            return File.ReadAllLines(filePath).Select(l => int.Parse(l)).ToArray();
+        }
+    }
+}
